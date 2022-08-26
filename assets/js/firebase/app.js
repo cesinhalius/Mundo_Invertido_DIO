273398e3058f084367d12959978628046ff5caf2
Cesar Augusto <cesinhalius@hotmail.com>
2022-08-26 18:58:33 -0300

    Adicionando conexão ao banco de dados com js

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDjIOez41cNwnCbBG5liL0da-BhCT7oLbM",
+    authDomain: "mundo-invertido-901f4.firebaseapp.com",
+    projectId: "mundo-invertido-901f4",
+    storageBucket: "mundo-invertido-901f4.appspot.com",
+    messagingSenderId: "385220999238",
+    appId: "1:385220999238:web:119df17941128324323247"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default app;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
-const Name = document.getElementById('txtName');
-const Email = document.getElementById('txtEmail');
-const Level = document.getElementById('txtLevel');
-const Character = document.getElementById('txtCharacter');
+import { subscribeToHellfireClub } from "./firebase/Hellfire_Club.js";
 
-const Subscribe = document.getElementById('btnSubscribe');
+const Name = document.getElementById("txtName");
+const Email = document.getElementById("txtEmail");
+const Level = document.getElementById("txtLevel");
+const Character = document.getElementById("txtCharacter");
+const Subscribe = document.getElementById("btnSubscribe");
 
+/* Adicionando no banco de dados*/
+Subscribe.addEventListener("click", async () => {
+  const subscription = {
+    name: Name.value,
+    email: Email.value,
+    level: Level.value,
+    character: Character.value,
+  };
 
-Subscribe.addEventListener('click', () => {
-    const subscription = {
-        name: Name.value,
-        email: Email.value,
-        level: Level.value,
-        character: Character.value
-    }
+  const subscriptionId = await subscribeToHellfireClub(subscription);
+  alert(`Inscrido com sucesso: ${subscriptionId}`);
+
+  Name.value = ''
+  Email.value = ''
+  Level.value = ''
+  Character.value = ''
 });
-
-
